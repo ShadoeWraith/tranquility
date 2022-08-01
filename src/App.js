0571833f65979374da3295cf8b7735d41afb47ad
Shaun Kiszonas <shadoewraith99@gmail.com>
2022-07-31 17:51:40 -0700

    fixed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route
-          path="/home"
+          path="/"
           element={
             <div>
               <Home />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,16 @@
-import { useState } from "react";
 import tranquility from "../assets/Tranquility.png";
-import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  const [currentCategory, setCurrentCategory] = useState(false);
-
-  const navCategories = [{ name: "home" }, { name: "members" }];
-
   return (
     <nav className="nav">
       <img src={tranquility} alt="free company logo" />
       <ul>
-        {navCategories.map((category) => (
-          <Link
-            className={`navbar ${
-              currentCategory.name === category.name ? "is-active" : ""
-            }`}
-            to={`/${category.name}`}
-            onClick={() => setCurrentCategory(category)}
-            key={category.name}
-          >
-            {category.name}
-          </Link>
-        ))}
+        <a href="/">
+          <li>Home</li>
+        </a>
+        <a href="/members">
+          <li>Members</li>
+        </a>
       </ul>
     </nav>
   );
